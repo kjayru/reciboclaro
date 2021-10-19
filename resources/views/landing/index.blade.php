f7dba910d17c7a7824dd199acfea438267aaed0e
havas-media <34104791+havas-media@users.noreply.github.com>
2021-10-19 11:13:07 -0500

    Update index.blade.php

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -72,6 +72,35 @@
         </div>
       </div>
 
+      <section class="section3">
+        <div class="section3__align">
+          <div class="section3__header">
+            <div class="title">
+              <h2>Nos importa mucho que entiendas tu recibo</h2>
+            </div>
+          </div>
+          <div class="section3__main">
+          <div class="info" style="padding-bottom:  0px;" >
+              <p>Si tienes dudas, te ayudamos a resolverlas</p>
+            </div>
+            <div class="item">
+               <!-- <div class="contenedor_video">
+                <div id="videoprom"></div>
+                <div class="capavideo">
+                  <div class="reload">
+                      <i class="fa fa-refresh" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div> -->
+
+              <span><a href="/dudas-sobre-tu-facturacion">Más información</a></span>
+            </div>
+            
+           
+          </div>
+        </div>
+      </section>
+
 
       <section class="section6">
         <div class="section6__align">
@@ -166,34 +195,7 @@
       </section> -->
 
       
-      <section class="section3">
-        <div class="section3__align">
-          <div class="section3__header">
-            <div class="title">
-              <h2>Nos importa mucho que entiendas tu recibo</h2>
-            </div>
-          </div>
-          <div class="section3__main">
-          <div class="info" style="padding-bottom:  0px;" >
-              <p>Si tienes dudas, te ayudamos a resolverlas</p>
-            </div>
-            <div class="item">
-               <!-- <div class="contenedor_video">
-                <div id="videoprom"></div>
-                <div class="capavideo">
-                  <div class="reload">
-                      <i class="fa fa-refresh" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div> -->
 
-              <span><a href="/dudas-sobre-tu-facturacion">Más información</a></span>
-            </div>
-            
-           
-          </div>
-        </div>
-      </section>
 
 
       <section class="section4">
