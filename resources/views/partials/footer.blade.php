d03f5a1f9c05f1fa38bec1cb0b3b5271c6c6d21d
havas-media <34104791+havas-media@users.noreply.github.com>
2022-01-07 10:25:05 -0500

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -28,94 +28,101 @@
         <!-- footer nuevo -->
 
         <div class="just__footer">
-        <div class="page1">
-          <div class="page1__main">
-            <section class="section1">
-              <div class="section1__align">
-                <div class="section1__header">
-                  <div class="content">
-                    <div class="content__list first">
-                      <div class="logo"><a href="https://www.claro.com.pe/" target="_blank"><img src="https://static.claro.com.pe/landings/havas/header-footer-final/v1/assets/logo_claro.svg" alt=""></a>
-                        <p>Todos los derechos reservados 2020</p>
+          <div class="page1">
+            <div class="page1__main">
+              <section class="section1">
+                <div class="section1__align">
+                  <div class="section1__header">
+                    <div class="block">
+                      <div class="block__list first">
+                        <div class="logo"><a href="https://www.claro.com.pe/" target="_blank"><img src="assets/logo_claro.svg" alt=""></a>
+                          <p>Todos los derechos reservados 2022</p>
+                        </div>
+                        <div class="social"><a class="ico_facebook" href="https://www.facebook.com/AmericaMovilPeruSAC" target="_blank"></a><a class="ico_instagram" href="https://www.instagram.com/claroperu/" target="_blank"></a><a class="ico_twitter" href="https://twitter.com/claroperu" target="_blank"></a><a class="ico_youtube" href="https://www.youtube.com/claroperu" target="_blank"></a></div>
+                        <div class="info">
+                          <blockquote>
+                            <p>Para ventas y servicios</p>
+                            <h4><a href="tel:0800 00200">0800 00200</a></h4>
+                            <div class="reclamaciones"><a href="https://www.claro.com.pe/atencion-de-reclamos/" target="_blank"><img src="assets/libro-de-reclamaciones-desktop.png" alt=""></a></div>
+                          </blockquote>
+                          <blockquote>
+                            <p>Para información</p>
+                            <h4><a href="tel:0800 00123">0800 00123</a></h4>
+                            <div class="reclamaciones"><a href="https://denuncias.americamovil.com/" target="_blank"><img src="assets/denuncias_azul.svg" alt=""></a></div>
+                          </blockquote>
+                        </div>
                       </div>
-                      <div class="social"><a class="ico_facebook" href="https://www.facebook.com/AmericaMovilPeruSAC" target="_blank"></a><a class="ico_instagram" href="https://www.instagram.com/claroperu/" target="_blank"></a><a class="ico_twitter" href="https://twitter.com/claroperu" target="_blank"></a><a class="ico_youtube" href="https://www.youtube.com/claroperu" target="_blank"></a></div>
-                      <div class="info">
-                        <blockquote>
-                          <p>Para ventas y servicios</p>
-                          <h4><a href="tel:0800 00200">0800 00200</a></h4>
-                        </blockquote>
-                        <blockquote>
-                          <p>Contáctanos</p>
-                          <h4><a href="tel:0800 00123">0800 00123</a></h4>
-                        </blockquote>
+                      <div class="block__list">
+                        <h3>De Utilidad<span class="btn_down"></span></h3>
+                        <ul>
+                          <li><a href="https://www.claro.com.pe/5g" target="_blank">Claro 5G</a></li>
+                          <li><a href="https://www.claro.com.pe/personas/movil/dias-claro-fans/" target="_blank">Días Claro Fans </a></li>
+                          <li><a href="https://www.claro.com.pe/consulta-de-numero/" target="_blank">Consulta de líneas</a></li>
+                          <li><a href="https://www.claro.com.pe/mide-tu-velocidad/" target="_blank">Mide tu velocidad</a></li>
+                          <li><a href="https://www.claro.com.pe/desbloquea-tu-celular/" target="_blank">Liberación de tu equipo móvil</a></li>
+                          <li><a href="https://www.claro.com.pe/comprobantes-electronicos/" target="_blank">Comprobantes electrónicos</a></li>
+                          <li><a href="https://static.claro.com.pe/img/otros/Llamada-por-llamada.pdf" target="_blank">Llamada por llamada</a></li>
+                        </ul>
+                      </div>
+                      <div class="block__list">
+                        <h3>Nos Hacemos Cargo<span class="btn_down"></span></h3>
+                        <ul>
+                          <li><a href="https://www.claro.com.pe/legal-y-regulatorio/devoluciones-por-interrupciones/" target="_blank">Devoluciones por interrupciones</a></li>
+                          <li><a href="https://www.claro.com.pe/atencion-de-reclamos/" target="_blank">Atención de reclamos</a></li>
+                          <li><a href="https://www.claro.com.pe/atencion-de-reclamos/consulta-el-estado-de-tu-reclamo/" target="_blank">Consulta de reclamos</a></li>
+                          <li><a href="https://static.claro.com.pe/img/otros/Comunicado-Adquirientes-iPhone-6-6S-y-SE.pdf" target="_blank">Adquirientes iPhone 6, 6S y SE</a></li>
+                          <li><a href="https://static.claro.com.pe/img/otros/Comunicado_a_Clientes_BlackBerry.pdf" target="_blank">Comunicado para clientes BlackBerry</a></li>
+                          <li><a href="https://www.claro.com.pe/no-al-phishing" target="_blank">Mensaje de Seguridad</a></li>
+                        </ul>
+                      </div>
+                      <div class="block__list">
+                        <h3>Institucional<span class="btn_down"></span></h3>
+                        <ul>
+                          <li><a href="https://www.claro.com.pe/institucional/sobre-nosotros/" target="_blank">Sobre nosotros</a></li>
+                          <li><a href="https://www.claro.com.pe/institucional/sostenibilidad/" target="_blank">Sostenibilidad</a></li>
+                          <li><a href="https://www.claro.com.pe/institucional/centro-de-prensa/" target="_blank">Centro de prensa</a></li>
+                          <li><a href="https://trabajaenclaro.claro.com.pe/" target="_blank">Trabaja en Claro</a></li>
+                          <li><a href="https://www.claro.com.pe/proveedores/" target="_blank">Proveedores</a></li>
+                          <li><a href="https://www.claro.com.pe/trabajos-de-mantenimiento/" target="_blank">Trabajos de mantenimiento</a></li>
+                          <li><a href="https://denuncias.americamovil.com/paginainicio.aspx" target="_blank">Portal de denuncias</a></li>
+                        </ul>
                       </div>
                     </div>
-                    <div class="content__list">
-                      <h3>De Utilidad<span class="btn_down"></span></h3>
-                      <ul>
-                        <li><a href="https://www.claro.com.pe/personas/estamos-juntos/fraccionamiento" target="_blank">Fracciona tu deuda</a></li>
-                        <li><a href="https://www.claro.com.pe/consulta-de-numero/" target="_blank">Consulta de líneas</a></li>
-                        <li><a href="https://www.claro.com.pe/mide-tu-velocidad/" target="_blank">Mide tu velocidad</a></li>
-                        <li><a href="https://www.claro.com.pe/desbloquea-tu-celular/" target="_blank">Liberación de tu equipo móvil</a></li>
-                        <li><a href="https://www.claro.com.pe/comprobantes-electronicos/" target="_blank">Comprobantes electrónicos</a></li>
-                        <li><a href="https://static.claro.com.pe/img/otros/Llamada-por-llamada.pdf" target="_blank">Llamada por llamada</a></li>
-                      </ul>
-                    </div>
-                    <div class="content__list">
-                      <h3>Nos Hacemos Cargo<span class="btn_down"></span></h3>
-                      <ul>
-                        <li><a href="https://www.claro.com.pe/legal-y-regulatorio/devoluciones-por-interrupciones/" target="_blank">Devoluciones por interrupciones</a></li>
-                        <li><a href="https://www.claro.com.pe/atencion-de-reclamos/" target="_blank">Atención de reclamos</a></li>
-                        <li><a href="https://www.claro.com.pe/atencion-de-reclamos/" target="_blank">Consulta de reclamos</a></li>
-                        <li><a href="https://www.claro.com.pe/atencion-de-reclamos/" target="_blank">Libro de reclamaciones</a></li>
-                      </ul>
-                    </div>
-                    <div class="content__list">
-                      <h3>Institucional<span class="btn_down"></span></h3>
-                      <ul>
-                        <li><a href="https://www.claro.com.pe/institucional/sobre-nosotros/" target="_blank">Sobre nosotros</a></li>
-                        <li><a href="https://www.claro.com.pe/institucional/sostenibilidad/" target="_blank">Sostenibilidad</a></li>
-                        <li><a href="https://www.claro.com.pe/trabajos-de-mantenimiento/" target="_blank">Trabajos de mantenimiento</a></li>
-                        <li><a href="https://www.claro.com.pe/institucional/centro-de-prensa/" target="_blank">Centro de prensa</a></li>
-                        <li><a href="https://denuncias.americamovil.com/paginainicio.aspx" target="_blank">Portal de denuncias</a></li>
-                      </ul>
+                  </div>
+                  <div class="section1__main">
+                    <div class="title">Legales<span class="btn_down"></span></div>
+                    <div class="block">
+                      <div class="block__list first">
+                        <ul>
+                          <li><a href="https://static.claro.com.pe/img/otros/Aviso_Legal_de_uso_del_sitio_web_08.pdf" target="_blank">Términos y condiciones de la web</a></li>
+                          <li><a href="https://www.claro.com.pe/garantia_equipos/" target="_blank">Condiciones de garantía de equipos</a></li>
+                        </ul>
+                      </div>
+                      <div class="block__list">
+                        <ul>
+                          <li><a href="https://www.claro.com.pe/proteccion-datos/" target="_blank">Política de Privacidad</a></li>
+                          <li><a href="https://www.claro.com.pe/legal-y-regulatorio/derechos-arco/" target="_blank">Derechos ARCO</a></li>
+                        </ul>
+                      </div>
+                      <div class="block__list">
+                        <ul>
+                          <li><a href="https://serviciosweb.osiptel.gob.pe/ConsultaSIRT/Buscar/frmConsultaTar.aspx" target="_blank">Sistema de consultas Tarifarias</a></li>
+                          <li><a href="https://static.claro.com.pe/img/otros/Medidas_Relativas_a_Neutralidad_de_Red_Octubre2021.pdf" target="_blank">Neutralidad de Red</a></li>
+                        </ul>
+                      </div>
+                      <div class="block__list">
+                        <ul>
+                          <li><a href="https://serviciosweb.osiptel.gob.pe/Devoluciones/" target="_blank">Sistema de Consulta de Deudas</a></li>
+                          <li><a href="https://www.claro.com.pe/legal-y-regulatorio/" target="_blank">Información abonados y usuarios</a></li>
+                        </ul>
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div class="section1__main">
-                  <div class="title">Legales<span class="btn_down"></span></div>
-                  <div class="content">
-                    <div class="content__list first">
-                      <ul>
-                        <li><a href="https://static.claro.com.pe/img/otros/Aviso_Legal_de_uso_del_sitio_web.pdf" target="_blank">Términos y condiciones de la web</a></li>
-                        <li><a href="https://www.claro.com.pe/garantia_equipos/" target="_blank">Condiciones de garantía de equipos</a></li>
-                      </ul>
-                    </div>
-                    <div class="content__list">
-                      <ul>
-                        <li><a href="https://www.claro.com.pe/proteccion-datos/" target="_blank">Política de Privacidad</a></li>
-                        <li><a href="https://www.claro.com.pe/legal-y-regulatorio/derechos-arco/" target="_blank">Derechos ARCO</a></li>
-                      </ul>
-                    </div>
-                    <div class="content__list">
-                      <ul>
-                        <li><a href="https://serviciosweb.osiptel.gob.pe/ConsultaSIRT/Buscar/frmConsultaTar.aspx" target="_blank">Sistema de consultas Tarifarias</a></li>
-                        <li><a href="https://static.claro.com.pe/img/otros/Medidas_Relativas_a_Neutralidad_implementadas_mayo_20.pdf" target="_blank">Neutralidad de Red</a></li>
-                      </ul>
-                    </div>
-                    <div class="content__list">
-                      <ul>
-                        <li><a href="https://serviciosweb.osiptel.gob.pe/Devoluciones/" target="_blank">Sistema de Consulta de Deudas</a></li>
-                        <li><a href="https://www.claro.com.pe/legal-y-regulatorio/" target="_blank">Información abonados y usuarios</a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
+              </section>
+            </div>
           </div>
         </div>
-      </div>
         
 
         <script src="/js/vendor/jquery.js"></script>
