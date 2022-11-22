90a411427e67990c2d9e5b063b79e5350fa193f2
Javi <ajgarrido@gmail.com>
2022-11-22 14:49:04 -0500

    Update debito.blade.php

--- resources/views/landing/debito.blade.php
+++ resources/views/landing/debito.blade.php
@@ -674,7 +674,7 @@ cargo con éxito. El importe máximo a devolver S/ 30*.</small>
                 <li> Si tienes una cuenta de ahorro en el BCP o Interbank, puedes escanear los documentos solicitados y enviarlos al buzón recaudaciondebitos@claro.com.pe.  </li>
                 <li> Para el caso de cuentas del BBVA y Scotiabank, se requiere acudir al Centro de Atención para presentar la documentación necesaria.  </li>
                 <li>  Para desafiliarte del débito automático debes hacerlo de manera telefónica o presencial. </li>
-                <li> Términos y condiciones sobre promoción 30% de dto. en tu primer recibo <a href="https://static.claro.com.pe/img/otros/bases-promocion-descuento-cargo-fijo-noviembre_2022.pdf" target="_blank">aquí</a></li>
+                <li> Términos y condiciones sobre promoción 30% de dto. en tu próximo recibo <a href="https://static.claro.com.pe/img/otros/bases-promocion-descuento-cargo-fijo-noviembre_2022.pdf" target="_blank">aquí</a></li>
                 {{-- <li> 
                  <!-- Términos y condiciones de campaña: "Gift card Visa" <a href="/assets/files/TyCDebitoautomaticogiftcardsVISA.pdf"/ target="_blank">aquí</a> -->
                 
